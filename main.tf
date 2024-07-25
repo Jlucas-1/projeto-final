1c79e87d00359d58082b4c529c1b0cdbc8c6cdcb
João Lucas <105443512+Jlucas-1@users.noreply.github.com>
2024-07-24 23:08:45 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,16 @@
 provider "azurerm" {
   features {}
 
-  # Variáveis para autenticação na Azure
-  subscription_id = var.azure_subscription_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
+  subscription_id = var.azure_subscription_id
   tenant_id       = var.azure_tenant_id
 }
+
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_subscription_id" {}
+variable "azure_tenant_id" {}
 
 # Criação do grupo de recursos
 resource "azurerm_resource_group" "resource_group" {
